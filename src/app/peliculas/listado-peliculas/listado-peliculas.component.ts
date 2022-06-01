2f4830458e87d70a632dfbf8d96ab7b4fdcceeb9
Víctor Martínez <ahal_tocob@hotmail.com>
2022-05-31 19:34:49 -0500

    Data

---
+++ src/app/peliculas/listado-peliculas/listado-peliculas.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listado-peliculas',
+  templateUrl: './listado-peliculas.component.html',
+  styleUrls: ['./listado-peliculas.component.css']
+})
+export class ListadoPeliculasComponent implements OnInit {
+  @Input()
+  peliculas: any;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  }
+
+  remover(index: number){
+
+  }
+}
