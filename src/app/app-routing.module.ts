0e42a18517e15153a0601dc246add2c1f44aa19c
Víctor Martínez <ahal_tocob@hotmail.com>
2022-06-01 14:58:05 -0500

    Ruteo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CrearActorComponent } from './actores/crear-actor/crear-actor.component';
+import { EditarActorComponent } from './actores/editar-actor/editar-actor.component';
+import { IndiceActoresComponent } from './actores/indice-actores/indice-actores.component';
+import { CrearCineComponent } from './cines/crear-cine/crear-cine.component';
+import { IndiceCinesComponent } from './cines/indice-cines/indice-cines.component';
+import { CrearGeneroComponent } from './generos/crear-genero/crear-genero.component';
+import { IndiceGenerosComponent } from './generos/indice-generos/indice-generos.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { CrearPeliculaComponent } from './peliculas/crear-pelicula/crear-pelicula.component';
 
 const routes: Routes = [
-  
+  { path: "", component: LandingPageComponent },
+
+  { path: "generos", component: IndiceGenerosComponent },
+  { path: "generos/crear", component: CrearGeneroComponent },
+
+  { path: "actores", component: IndiceActoresComponent },
+  { path: "actores/crear", component: CrearActorComponent },
+  { path: "actores/editar/:id", component: EditarActorComponent },
+
+  { path: "cines", component : IndiceCinesComponent},
+  { path: "cines/crear", component: CrearCineComponent},
+
+  { path: "pelicular/crear", component: CrearPeliculaComponent},
+
+  { path: "**", redirectTo: "" }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,15 @@ import {MaterialModule} from './material/material.module';
 import { MenuComponent } from './menu/menu.component';
 import { RatingComponent } from './utilidades/rating/rating.component';
 import { ListadoGenericoComponent } from './utilidades/listado-generico/listado-generico.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { IndiceGenerosComponent } from './generos/indice-generos/indice-generos.component';
+import { CrearGeneroComponent } from './generos/crear-genero/crear-genero.component';
+import { IndiceActoresComponent } from './actores/indice-actores/indice-actores.component';
+import { CrearActorComponent } from './actores/crear-actor/crear-actor.component';
+import { CrearPeliculaComponent } from './peliculas/crear-pelicula/crear-pelicula.component';
+import { IndiceCinesComponent } from './cines/indice-cines/indice-cines.component';
+import { CrearCineComponent } from './cines/crear-cine/crear-cine.component';
+import { EditarActorComponent } from './actores/editar-actor/editar-actor.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +25,16 @@ import { ListadoGenericoComponent } from './utilidades/listado-generico/listado-
     ListadoPeliculasComponent,
     MenuComponent,
     RatingComponent,
-    ListadoGenericoComponent
+    ListadoGenericoComponent,
+    LandingPageComponent,
+    IndiceGenerosComponent,
+    CrearGeneroComponent,
+    IndiceActoresComponent,
+    CrearActorComponent,
+    CrearPeliculaComponent,
+    IndiceCinesComponent,
+    CrearCineComponent,
+    EditarActorComponent
   ],
   imports: [
     BrowserModule,
